89d7a0d93f674a2e88638164b2d06fcd75465f58
thyngb <89824483+thyngb@users.noreply.github.com>
2023-02-23 09:09:48 +0800

    dsas

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-// Register a callback function to be called whenever a new file is created
-app.vault.on("create", async (file) => {
-    // Retrieve the contents of the new file
-    const contents = await app.vault.read(file);
+'use strict';
+
+const { Plugin } = require('obsidian');
+
+class MyPlugin extends Plugin {
+    onload() {
+      // Register a callback function to be called whenever a new file is created
+      this.registerEvent(this.app.vault.on("create", async (file) => {
+        // Retrieve the contents of the new file
+        const contents = await this.app.vault.read(file);
   
-    // Do something with the file contents
-    console.log(`New file created: ${file.path}`);
-    console.log(`Contents: ${contents}`);
-});
+        // Display a popup with the file path and contents
+        new Notice(`New file created: ${file.path}\nContents: ${contents}`);
+      }));
+    }
+  }
+  
+  module.exports = MyPlugin;
+  
   
